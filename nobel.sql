d5cebcde09c7cbbcd84db4c63601932e01adfc66
Alain2701 <36484445+Alain2701@users.noreply.github.com>
2018-10-15 07:58:47 -0700

    Create nobel.sql

---
+++ nobel.sql
@@ -0,0 +1,75 @@
+# sqlzoo-trabajos
+
+1.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+ 
+ 2. 
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+   
+   3.
+   SELECT yr, Subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein'
+ 
+ 4.
+ SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+ 
+ 5.
+ SELECT yr, subject, winner
+FROM nobel
+WHERE (yr >=1980 AND yr <=1989) AND subject = 'Literature'
+
+6.
+SELECT *
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+
+7.
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'john%'
+
+8.
+SELECT *
+FROM nobel
+WHERE (subject = 'Physics' AND yr = '1980') OR (subject = 'Chemistry' AND yr = 1984)
+
+9.
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+10. 
+SELECT *
+FROM nobel
+WHERE (subject  = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
+
+11. 
+SELECT *
+FROM nobel
+WHERE winner LIKE 'PETER GRÜNBERG'
+
+12.
+SELECT *
+FROM nobel
+WHERE winner LIKE 'EUGENE O''NEILL'
+
+13.
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'sir%'
+ORDER BY yr DESC, winner
+
+14.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+ 
